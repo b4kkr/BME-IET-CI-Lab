ab6fb8ad8bb6dfebc313f3ddc256cd7cc6882ff7
Bakk Richard <b4kk@icloud.com>
2020-05-07 13:43:34 +0200

    Fix at random

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,5 +1,6 @@
 package hu.bme.mit.spaceship;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 /**
@@ -9,8 +10,7 @@ import java.util.Random;
 */
 public class TorpedoStore {
 
-  Random generator = new Random();
-
+  Random generator;
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
 
@@ -18,7 +18,11 @@ public class TorpedoStore {
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
-
+    try {
+      generator = SecureRandom.getInstanceStrong();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     // update failure rate if it was specified in an environment variable
     String failureEnv = System.getenv("IVT_RATE");
     if (failureEnv != null){
